8d8d377f7a4347ad774222673d08fa01597683e2
Carlos Villanueva <ceva_19@hotmail.com>
2018-11-22 23:11:31 -0300

    JavaServer Faces - Chapter 03 - Restful Dymanic Navigation

---
+++ ch03/ch03-RestFul-Navigation/src/main/java/com/corejsf/Problem.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf;
+
+import java.io.Serializable;
+
+public class Problem implements Serializable {
+
+    private String question;
+    private String answer;
+
+    public Problem(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    // override for more sophisticated checking
+    public boolean isCorrect(String response) {
+        return response.trim().equalsIgnoreCase(answer);
+    }
+}
