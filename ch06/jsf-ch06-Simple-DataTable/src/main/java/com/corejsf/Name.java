3f205f64bd7180514f761ca8172b2669d84ec1f8
Carlos Villanueva <ceva_19@hotmail.com>
2018-12-27 22:05:35 -0300

    Java Core JSF - Chapter 6 - Data Table

---
+++ ch06/jsf-ch06-Simple-DataTable/src/main/java/com/corejsf/Name.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author PC
+ */
+public class Name implements Serializable{
+    private String first;
+    private String last;
+    
+    public Name(String first, String last)
+    {
+        this.first = first;
+        this.last = last;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+    
+    
+}
---
+++ ch06/jsf-ch06-Simple-DataTable/src/main/java/com/corejsf/TableData.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author PC
+ */
+@Named(value = "tableData")
+@SessionScoped
+public class TableData implements Serializable {
+
+    private static final Name[] names = new Name[]
+    {
+        new Name("William", "Dupont"),
+        new Name("Anna", "Keeney"),
+        new Name("Mariko", "Randor"),
+        new Name("John", "Wilson")
+    };
+    
+    public TableData() {
+    }
+    
+    public Name[] getNames()
+    {
+        return names;
+    }
+    
+}
