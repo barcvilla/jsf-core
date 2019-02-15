c2ae0c0f7f7ba667a30bc2234bc38a6860c03419
Carlos Villanueva <ceva_19@hotmail.com>
2019-02-15 15:22:30 -0300

    JavaServer Faces By Examples - Chapter 3 - Navigation Pages

---
+++ jsf-by-example/ch03/jsf-sample-ch03-04-1-NavigationPages/src/main/java/com/jsf/controller/NavigationController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jsf.controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author PC
+ */
+@Named(value = "navigationController")
+@SessionScoped
+public class NavigationController implements Serializable {
+
+    private boolean authenticated = false;
+    
+    public NavigationController() {
+    }
+    
+    public String pageOne()
+    {
+        return "PAGE_1";
+    }
+    
+    public String pageTwo()
+    {
+        return "PAGE_2";
+    }
+    
+    public String nextPage()
+    {
+        return "navigation2";
+    }
+
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    public void setAuthenticated(boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+    
+    public void login()
+    {
+        setAuthenticated(true);
+        System.out.println("Here");
+    }
+}
