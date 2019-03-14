0c7c40cb755dc28f94bd0e23af176e260c144198
Carlos Villanueva <ceva_19@hotmail.com>
2019-03-14 12:41:06 -0300

    JavaServer Faces - Chapter 3 - Displaying List Objects

---
+++ jsf-by-example/ch03/jsf-sample-ch03-10-BookmarkableUrl/src/main/java/com/corejsf/controller/AuthorTableController.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf.controller;
+
+import com.corejsf.domain.Author;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author PC
+ */
+@Named(value = "authorTableController")
+@SessionScoped
+public class AuthorTableController implements Serializable {
+
+    private List<Author> authorList = null;
+    private final String juneauBio = "This is Josh Juneau's Bio";
+    private final String deaBio = "This is Carl Dea's Bio";
+    private final String beatyBio = "This is Mark Beaty's Bio";
+    private final String oConnerBio = "This is John O'Connor's Bio";
+    private final String guimeBio = "This is Freddy Guime's Bio";
+    private Author current;
+    private String authorLast;
+
+    public AuthorTableController() {
+        super();
+        authorLast = null;
+        populateAuthorList();
+    }
+
+    private void populateAuthorList() {
+
+        if (authorList == null) {
+            System.out.println("initializng authors list");
+            authorList = new ArrayList<>();
+            authorList.add(new Author("Josh", "Juneau", juneauBio));
+            authorList.add(new Author("Carl", "Dea", deaBio));
+            authorList.add(new Author("Mark", "Beaty", beatyBio));
+            authorList.add(new Author("John", "O'Conner", oConnerBio));
+            authorList.add(new Author("Freddy", "Guime", guimeBio));
+        }
+    }
+
+    public String displayAuthor(String last) {
+        for (Author author : authorList) {
+            if (author.getLast().equals(last)) {
+                current = author;
+                break;
+            }
+        }
+        return "passingParameters2";
+    }
+    
+    public List getAuthorList() {
+        System.out.println("Getting the authorlist =>" + authorList.size());
+        return authorList;
+    }
+    
+    public Author getCurrent() {
+        return current;
+    }
+    
+    public void setCurrent(Author current) {
+        this.current = current;
+    }
+    
+    public String getAuthorLast() {
+        return authorLast;
+    }
+    
+    public void setAuthorLast(String authorLast)
+    {
+        System.out.println("se ejecuto el metodo set");
+        displayAuthor(authorLast);
+    }
+}
---
+++ jsf-by-example/ch03/jsf-sample-ch03-11-DisplayigListObjs/src/main/java/com/corejsf/domain/Author.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf.domain;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author PC
+ */
+public class Author implements Serializable{
+    private String first;
+    private String last;
+    private String bio;
+    
+    public Author()
+    {
+        this.first = null;
+        this.last = null;
+        this.bio = null;
+    }
+    
+    public Author(String first, String last, String bio)
+    {
+        this.first = first;
+        this.last = last;
+        this.bio = bio;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+    
+    
+}
